5042236607fe08d6ca2f89f50ec0d2a49892bd82
Emory Merryman <emory.merryman@gmail.com>
2016-11-03 23:44:00 +0000

    working on it

--- install.sh
+++ install.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 
-apk update &&
+echo source http://www.spontaneoussymmetry.com/blog/2012-12-18-blaze
+    apk update &&
     apk upgrade &&
+    echo Move to a clean directory &&
+    mkdir /usr/local/src &&
+    cd /usr/local/src &&
+    echo Create a local virtual environment and then activate it. &&
     apk add python &&
     apk add py-pip &&
+    pip install --upgrade pip &&
     pip install virtualenv &&
-    cd /usr/local/src &&
     virtualenv venv --distribute &&
     . venv/bin/activate &&
-    git clone git@github.com:ContinuumIO/conda.git &&
+    echo Checkout the 'conda' build system &&
+    apk add git &&
+    git clone https://github.com/conda/conda.git &&
     cd conda &&
-    python setup.sh build &&
-    python setup.sh install &&
+    python setup.py build &&
+    python setup.py install &&
     cd .. &&
     mkdir venv/pkgs &&
     cd conda &&
-    ./bin/conda install ply &&
-    ./bin/conda install blosc &&
-    ./bin/conda install aterm &&
+    ./conda install ply &&
+    ./conda install blosc &&
+    ./conda install aterm &&
     pip install Cython &&
     pip install numpy &&
     pip install numexpr &&
     export DYLD_LIBRARY_PATH=PWD/venv/lib:PWD/venv/lib:{DYLD_LIBRARY_PATH} &&
     export LD_LIBRARY_PATH=PWD/venv/lib:PWD/venv/lib:{LD_LIBRARY_PATH} &&
     git clone git@github.com:ContinuumIO/blaze.git &&
-    cd blaze &&
+    cd /usr/local/src &&
     make build &&
     make docs &&
     python setup.py install &&
